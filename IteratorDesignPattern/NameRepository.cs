859f3d4388fa55cb843e1bffe74f05ee75178f55
Karan Kanchan <karan.kanchan@morningstar.com>
2021-09-01 22:25:55 +0530

    Implemented Iterator Design Pattern

---
+++ IteratorDesignPattern/NameRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorDesignPattern
+{
+    public class NameRepository : IContainer
+    {
+        public static string[] names = { "Robert", "Karan","Sagar","Bhavesh" };
+        public IIterator getIterator()
+        {
+            return new NameIterator();
+
+        }
+
+
+
+        public class NameIterator : IIterator
+        {
+
+            int index;
+
+            public bool hasNext()
+            {
+                if (index < names.Length)
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            public object next()
+            {
+                if (this.hasNext())
+                {
+                    return names[index++];
+                }
+                return null;
+            }
+        }
+    }
+}
---
+++ IteratorDesignPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IteratorDesignPattern
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            NameRepository namesRepository = new NameRepository();
+
+            for (IIterator iter = namesRepository.getIterator(); iter.hasNext();)
+            {
+                String name = (String)iter.next();
+                Console.WriteLine("Name : " + name);
+            }
+        }
+
+    }
+
+}
